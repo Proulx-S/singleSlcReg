28f6fc84b6c8021fc1401286038f12e2e8261bd8
Proulx-S <sebastien.proulx2@mail.mcgill.ca>
2022-10-31 12:36:34 -0400

    Initiate scripts

---
+++ try_2dImReg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export PATH=$PATH:/usr/pubsw/packages/AFNI/21.0.21
+export FREESURFER_HOME=/usr/local/freesurfer/7.3.3; source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+# Copy example data from raw dir to here
+
+# Try 2dImReg
